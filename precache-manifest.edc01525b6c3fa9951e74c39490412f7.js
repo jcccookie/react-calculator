e7845f6a2e1c16709b96bcf61647e70a579b1da5
jcccookie <doublekn88@gmail.com>
2019-08-06 23:31:25 -0700

    Updates

---
+++ precache-manifest.edc01525b6c3fa9951e74c39490412f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada274c82827f2e4ab99bd32d65ea55d",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "ab697aa388898a55605a",
+    "url": "/react-calculator/static/css/main.10eb28a7.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-calculator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ab697aa388898a55605a",
+    "url": "/react-calculator/static/js/main.746da287.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
